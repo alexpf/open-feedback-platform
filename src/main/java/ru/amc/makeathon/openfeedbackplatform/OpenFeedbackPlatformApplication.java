8d9608119be48e1a292b6d04a77472f202ec3f71
Aleksei Lesnov <aleksei_lesnov@epam.com>
2019-02-26 11:29:08 +0300

    Initial project template

---
+++ src/main/java/ru/amc/makeathon/openfeedbackplatform/OpenFeedbackPlatformApplication.java
@@ -0,0 +1,13 @@
+package ru.amc.makeathon.openfeedbackplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFeedbackPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenFeedbackPlatformApplication.class, args);
+	}
+
+}
